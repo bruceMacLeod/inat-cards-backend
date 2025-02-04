a934f5c21fe3d31e7091b8c2c3094d97b80f63e2
Bruce MacLeod <macleod@maine.edu>
2025-02-03 20:56:16 -0500

    Updated data files

--- app.py
+++ app.py
@@ -186,8 +186,9 @@ def load_cards():
 def load_csv_data(file_path: str) -> Optional[pd.DataFrame]:
     """Load CSV data and return DataFrame."""
     try:
-        data = pd.read_csv(file_path)[["image_url", "scientific_name", "common_name","taxa_url","observer_name",
-                                       "observation_year","observation_url"]].dropna()
+# ,"observer_name",
+#                                        "observation_year","observation_url"
+        data = pd.read_csv(file_path)[["image_url", "scientific_name", "common_name","taxa_url","observer_name","observation_year","observation_url"]].dropna()
         return data
     except Exception as e:
         logger.error(f"Error loading CSV: {str(e)}")
